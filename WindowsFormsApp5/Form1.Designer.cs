152482b7bdf46948e9b207a6d86b4fea5af2dc8f
zalevskayaa.anya@gmail.com <zalevskayaa.anya@gmail.com>
2023-07-20 14:54:15 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp5/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxXmin = new System.Windows.Forms.TextBox();
+            this.textBoxXmax = new System.Windows.Forms.TextBox();
+            this.textBoxStep = new System.Windows.Forms.TextBox();
+            this.button_calc = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listBoxPoints = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.Color.Transparent;
+            this.chart1.BorderlineColor = System.Drawing.Color.Black;
+            chartArea2.AxisX.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.True;
+            chartArea2.AxisX.MajorGrid.Enabled = false;
+            chartArea2.AxisX2.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.False;
+            chartArea2.AxisY.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.True;
+            chartArea2.AxisY.MajorGrid.Enabled = false;
+            chartArea2.AxisY2.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.False;
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(3, 68);
+            this.chart1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Fire;
+            series3.BorderWidth = 3;
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series3.Legend = "Legend1";
+            series3.MarkerColor = System.Drawing.Color.LightCyan;
+            series3.Name = "Заданная функция";
+            series4.BorderWidth = 3;
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series4.Legend = "Legend1";
+            series4.Name = "Прямая";
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(800, 313);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Malgun Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(79, 386);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Xmin";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Malgun Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(217, 382);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Xmax";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Malgun Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(361, 382);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 23);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Шаг";
+            // 
+            // textBoxXmin
+            // 
+            this.textBoxXmin.Location = new System.Drawing.Point(71, 416);
+            this.textBoxXmin.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBoxXmin.Name = "textBoxXmin";
+            this.textBoxXmin.Size = new System.Drawing.Size(100, 22);
+            this.textBoxXmin.TabIndex = 5;
+            // 
+            // textBoxXmax
+            // 
+            this.textBoxXmax.Location = new System.Drawing.Point(208, 416);
+            this.textBoxXmax.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBoxXmax.Name = "textBoxXmax";
+            this.textBoxXmax.Size = new System.Drawing.Size(100, 22);
+            this.textBoxXmax.TabIndex = 6;
+            // 
+            // textBoxStep
+            // 
+            this.textBoxStep.Location = new System.Drawing.Point(351, 416);
+            this.textBoxStep.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBoxStep.Name = "textBoxStep";
+            this.textBoxStep.Size = new System.Drawing.Size(100, 22);
+            this.textBoxStep.TabIndex = 7;
+            // 
+            // button_calc
+            // 
+            this.button_calc.BackColor = System.Drawing.Color.Snow;
+            this.button_calc.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button_calc.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_calc.Font = new System.Drawing.Font("Malgun Gothic Semilight", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_calc.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button_calc.Location = new System.Drawing.Point(517, 400);
+            this.button_calc.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button_calc.Name = "button_calc";
+            this.button_calc.Size = new System.Drawing.Size(151, 37);
+            this.button_calc.TabIndex = 8;
+            this.button_calc.Text = "Рассчитать";
+            this.button_calc.UseVisualStyleBackColor = false;
+            this.button_calc.Click += new System.EventHandler(this.button_calc_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::WindowsFormsApp5.Properties.Resources.Снимок_экрана__196_;
+            this.pictureBox1.Location = new System.Drawing.Point(260, 12);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(216, 58);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.WaitOnLoad = true;
+            // 
+            // listBoxPoints
+            // 
+            this.listBoxPoints.FormattingEnabled = true;
+            this.listBoxPoints.ItemHeight = 16;
+            this.listBoxPoints.Location = new System.Drawing.Point(601, 137);
+            this.listBoxPoints.Name = "listBoxPoints";
+            this.listBoxPoints.Size = new System.Drawing.Size(175, 228);
+            this.listBoxPoints.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WindowsFormsApp5.Properties.Resources.stats_uptrend_shutterstock_687485104_1_original;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBoxPoints);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button_calc);
+            this.Controls.Add(this.textBoxStep);
+            this.Controls.Add(this.textBoxXmax);
+            this.Controls.Add(this.textBoxXmin);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxXmin;
+        private System.Windows.Forms.TextBox textBoxXmax;
+        private System.Windows.Forms.TextBox textBoxStep;
+        private System.Windows.Forms.Button button_calc;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox listBoxPoints;
+    }
+}
+
---
+++ WindowsFormsApp5/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_calc_Click(object sender, EventArgs e)
+        {
+            if (textBoxStep.Text != string.Empty && textBoxXmax.Text != string.Empty && textBoxXmin.Text != string.Empty)
+            {
+                double Xmin = 0;
+                Xmin = GetNum(textBoxXmin, Xmin);
+                double Xmax = 0;
+                Xmax = GetNum(textBoxXmax, Xmax);
+                double Step = 0;
+                Step = GetNum(textBoxStep, Step);
+                double b = -0.8;
+                if (Xmax > Xmin && Step > 0)
+                {
+                    double kolPointer = (Xmax - Xmin) / Step + 1;
+                    int count = (int)Math.Ceiling(kolPointer);
+
+                    double[] x = new double[count];
+
+                    double[] y1 = new double[count];
+                    double[] y2 = new double[count];
+
+                    for (int i = 0; i < count; i++)
+                    {
+                        x[i] = Xmin + Step * i;
+                        y1[i] = Math.Pow(x[i], 2) + Math.Tan(5 * x[i] + b / x[i]);
+                        y2[i] = x[i];
+                        listBoxPoints.Items.Add("[" + x[i] + "; " + y1[i] + "]");
+                    }
+
+                    chart1.ChartAreas[0].AxisX.Minimum = Xmin;
+                    chart1.ChartAreas[0].AxisX.Maximum = Xmax;
+
+                    chart1.ChartAreas[0].AxisX.MajorGrid.Interval = Step;
+
+                    chart1.Series[0].Points.DataBindXY(x, y1);
+                    chart1.Series[1].Points.DataBindXY(x, y2);
+                }
+                else
+                {
+                    MessageBox.Show("Введены невозможные значения!");
+                }
+            }
+            else { MessageBox.Show("Значения не введены!"); }
+        }
+        private double GetNum(TextBox textBox, double x)
+        {
+            if(!double.TryParse(textBox.Text, out x))
+            {
+                MessageBox.Show("Значение некорректно!");
+            }
+            return x;
+        }
+    }
+}
